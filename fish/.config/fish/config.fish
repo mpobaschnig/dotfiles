954f9135d75f0f3f54f9091e04dca310b412bf6c
Martin Pobaschnig <mpobaschnig@protonmail.com>
2024-10-21 20:10:17 +0200

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Disable greeting :(
+set fish_greeting
+
+# Aliases 
+alias lg "lazygit $argv"
+alias nv "nvim $argv"
+alias up "rpm-ostree update && flatpak update -y"
